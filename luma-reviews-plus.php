430ea322cc876e70d08db868426a6278320f743c
Terje Johansen <terje@hello-world.no>
2026-05-07 15:30:30 +0200

    Version fuckup. Just bumping

--- luma-reviews-plus.php
+++ luma-reviews-plus.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Plugin Name: Luma Reviews Plus
- * Description: Tokenized WooCommerce review requests with native product reviews and verified shop-experience reviews.
- * Version: 0.2.1
+ * Description: WooCommerce review requests with native product reviews and verified shop-experience reviews.
+ * Version: 0.2.2
  * Plugin URI: https://github.com/luma-retail/luma-reviews-plus
  * Author: Terje Johansen
  * Requires Plugins: woocommerce
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'LUMA_REVIEWS_PLUS_VERSION', '0.2.1' );
+define( 'LUMA_REVIEWS_PLUS_VERSION', '0.2.2' );
 define( 'LUMA_REVIEWS_PLUS_FILE', __FILE__ );
 define( 'LUMA_REVIEWS_PLUS_PATH', \plugin_dir_path( __FILE__ ) );
 define( 'LUMA_REVIEWS_PLUS_URL', \plugin_dir_url( __FILE__ ) );
